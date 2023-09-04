7e1651deddfc22c74cb0e9af4d33e4a101433821
TCAM\GIS0938 <alfredo.aguilar@movistar.com.sv>
2023-09-04 01:44:22 -0600

    tabla de clientes funcional

---
+++ client/src/pages/ClientesPage.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+import { useCliente } from "../context/ClientesContext";
+import {TablaCliente} from '../components/TablaCliente'
+
+export function ClientesPage() {
+  const { obtenerClientes, clientes } = useCliente();
+  useEffect(() => {
+    obtenerClientes();
+  }, []);
+  console.log(clientes);
+  return (
+    <>
+      <h1 className="text-center">Pagina de Clientes</h1>
+      
+      <TablaCliente clientes={clientes}/>
+    </>
+  );
+}
